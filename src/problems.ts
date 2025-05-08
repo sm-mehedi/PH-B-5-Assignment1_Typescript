45107263e272207f88aaeb3f66d177cc02a582da
sm-mehedi <hasanmehedi63395@gmail,com>
2025-05-08 09:56:18 +0600

    2 problems has been solved

--- src/problems.ts
+++ src/problems.ts
@@ -1,4 +1,36 @@
 {
-    const meaow:string = 'Iam a meaow';
-    console.log(meaow);
+    function formatString(input: string, toUpper?: boolean): string {
+        if (toUpper || toUpper == undefined) {
+            return input.toUpperCase()
+            
+        }
+        else{
+            return input.toLowerCase()
+        }
+    }
+    console.log(formatString("Hello"));
+    console.log(formatString("Hello",true));
+    console.log(formatString("Hello",false));
+
+    const books = [
+        { title: "Book A", rating: 4.5 },
+        { title: "Book B", rating: 3.2 },
+        { title: "Book C", rating: 5.0 }
+      ];
+
+      function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+        return items.filter(item => item.rating >= 4);
+      }
+      
+      console.log(filterByRating(books));
+
+
+      
+
+
+
+
+
+
+
 }
